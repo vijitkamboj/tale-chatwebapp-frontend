d149b0d6dfbae47a968d7a7f574e4dc6546323fd
Vijit Kamboj <vijitkamboj92@gmail.com>
2019-06-02 07:52:12 +0530

    set-up project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
+
+import {BrowserRouter as Router , Switch , Route } from 'react-router-dom';
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
