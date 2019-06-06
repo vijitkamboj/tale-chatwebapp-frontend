f07688753c3f5d4589d9b35cfe02f5ce4b7ef9ba
Vijit Kamboj <vijitkamboj92@gmail.com>
2019-06-06 20:47:37 +0530

    fixed logo error

--- src/containers/Auth/logo.js
+++ src/containers/Auth/logo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.png'
 
-const Logo = ({Link}) => <Link to = "/" style={{margin : "8px" , alignSelf : "flex-start"}}>
+const Logo = ({Link}) => <Link to = "/" style={{margin : "8px" , alignSelf : "flex-start" , backgroundColor: "rgb(121, 120, 120,0.2)"}}>
                             <img 
                             src={logo} 
                             alt="logo" 
