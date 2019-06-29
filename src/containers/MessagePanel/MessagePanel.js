6bb633a70e6045a493510bef89974a4a09006509
Vijit Kamboj <vijitkamboj92@gmail.com>
2019-06-29 09:33:49 +0530

    started developing messages segement

--- src/containers/MessagePanel/MessagePanel.js
+++ src/containers/MessagePanel/MessagePanel.js
@@ -1,10 +1,23 @@
 import "./MessagePanel.css"
 import React, { Component } from 'react';
+import MessagesHeader from "./MessagesHeader/MessagesHeader";
+import MessagesForm from "./MessagesForm/MessagesForm"
+import {Segment,Comment} from "semantic-ui-react"
 
 class MessagePanel extends Component{
     render(){
         return(
-            <div id="message-panel" className="panels"></div>
+            <div id="message-panel" className="panels">
+                <MessagesHeader />
+
+                <Segment>
+                    <Comment.Group>
+
+                    </Comment.Group>
+                </Segment>
+
+                <MessagesForm />
+            </div>
         )
     }
 }
---
+++ src/containers/MessagePanel/MessagesHeader/MessagesHeader.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import "./MessagesHeader.css"
+
+
+
+class MessagesHeader extends Component{
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default MessagesHeader
